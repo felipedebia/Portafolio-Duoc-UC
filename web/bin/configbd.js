35d329eba0ff142ca708e07b839496a06a443689
Mariio PC <fabyold@gmail.com>
2021-10-19 23:36:49 -0300

    80

--- web/bin/configbd.js
+++ web/bin/configbd.js
@@ -1,22 +1,16 @@
 const oracledb = require('oracledb');
 
 config = {
-    user: "portafolio",
+    user: "portafolio1",
     password: "portafolio",
     connectString: "localhost:1521/xe"
 }
 
 async function Open(sql, binds, autoCommit) {
-    let cnn = await oracledb.getConnection(config);
     console.log('[!] Trying to connect to database');
+    let cnn = await oracledb.getConnection(config);
+    console.log('[!] Connected to database');
     let result = await cnn.execute(sql, binds, { autoCommit });
-
-    //if(result.status(200)) {
-
-    //} else {
-
-    //}
-
     console.log('[!] Query executed');
     cnn.release();
     return result;
