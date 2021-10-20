527975a85242e663f5caed9df3725b280d545387
Mariio PC <fabyold@gmail.com>
2021-10-19 23:54:08 -0300

    81

--- web/routes/api.js
+++ web/routes/api.js
@@ -85,9 +85,9 @@ router.post('/crearUsuario', async (req, res) => {
 // Modificar
 router.post("/modificarUsuario/:id_usuario", async (req, res) => {
   const { id_usuario } = req.params;
-  const { num_documento, tipo_usuario, nombre, apellido, fecha_nacimiento, genero, correo, estado_cuenta, telefono, password } = req.body;
-  sql = "UPDATE usuario SET correo= :correo, nombre= :nombre, apellido= :apellido, num_documento= :num_documento, tipo_usuario= :tipo_usuario, fecha_nacimiento= :fecha_nacimiento, genero= :genero, estado_cuenta= :estado_cuenta, telefono= :telefono, password= :password WHERE id_usuario= :id_usuario";
-  await BD.Open(sql, [num_documento, tipo_usuario, nombre, apellido, fecha_nacimiento, genero, correo, estado_cuenta, telefono, password, id_usuario], true);
+  const { num_documento, tipo_usuario, nombre, apellido, fecha_nacimiento, genero, correo, estado_cuenta, telefono} = req.body;
+  sql = "UPDATE usuario SET correo= :correo, nombre= :nombre, apellido= :apellido, num_documento= :num_documento, tipo_usuario= :tipo_usuario, fecha_nacimiento= :fecha_nacimiento, genero= :genero, estado_cuenta= :estado_cuenta, telefono= :telefono WHERE id_usuario= :id_usuario";
+  await BD.Open(sql, [num_documento, tipo_usuario, nombre, apellido, fecha_nacimiento, genero, correo, estado_cuenta, telefono, id_usuario], true);
 
   // Si tuvo conexión a la DB
   if(res.status(200)) {
